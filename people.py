ba9d9491b8e9c4286e61ea226f8d22c9ff14b86f
caustin1118 <76984895+caustin1118@users.noreply.github.com>
2021-10-29 22:10:26 -0400

    Movie & people profiles for model exploration

---
+++ people.py
@@ -0,0 +1,40 @@
+class Person1:
+    def personAge(self):
+        age = 17
+        return age
+
+    def favouriteGenre(self):
+        genre = "Romance"
+        return genre
+
+    def availableTimeInHours(self):
+        timeInHours = 2
+        return timeInHours
+
+
+class Person2:
+    def age(self):
+        age = 23
+        return age
+
+    def favouriteGenre(self):
+        genre = "Action"
+        return genre
+
+    def availableTimeInHours(self):
+        timeInHours = 3
+        return timeInHours
+
+
+class Person3:
+    def age(self):
+        age = 14
+        return age
+
+    def favouriteGenre(self):
+        genre = "Action"
+        return genre
+
+    def availableTimeInHours(self):
+        timeInHours = 2
+        return timeInHours
